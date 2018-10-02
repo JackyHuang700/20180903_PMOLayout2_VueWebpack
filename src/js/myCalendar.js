300d1236be1c7b645bede84f7858b75017b9f05d
Jacky <jacky8595125@gmail.com>
2018-10-02 11:18:45 +0800

    ...

---
+++ src/js/myCalendar.js
@@ -0,0 +1,140 @@
+(function () {
+  var data = [{
+    title: 'All Day Event',
+    start: '2015-02-01'
+  }, {
+    title: 'Long Event',
+    start: '2015-02-07',
+    end: '2015-02-10'
+  }, {
+    id: 999,
+    title: 'Repeating Event',
+    start: '2015-02-09T16:00:00'
+  }, {
+    id: 999,
+    title: 'Repeating Event',
+    start: '2015-02-16T16:00:00'
+  }, {
+    title: 'Conference',
+    start: '2015-02-11',
+    end: '2015-02-13'
+  }, {
+    title: 'Meeting',
+    start: '2015-02-12T10:30:00',
+    end: '2015-02-12T12:30:00'
+  }, {
+    title: 'Lunch',
+    start: '2015-02-12T12:00:00'
+  }, {
+    title: 'Meeting',
+    start: '2015-02-12T14:30:00'
+  }, {
+    title: 'Happy Hour',
+    start: '2015-02-12T17:30:00'
+  }, {
+    title: 'Dinner',
+    start: '2015-02-12T20:00:00'
+  }, {
+    title: 'Birthday Party',
+    start: '2015-02-13T07:00:00'
+  }, {
+    title: 'Click for Google',
+    url: 'https://google.com/',
+    start: '2015-02-28'
+  }];
+  var newData = [{
+    title: 'stuff',
+    start: '2015-03-01'
+  }, {
+    title: 'stuff',
+    start: '2015-03-02'
+  }];
+  var popTemplate = ['<div class="popover" style="max-width:600px;" >', '<div class="arrow"></div>', '<div class="popover-header">', '<button id="closepopover" type="button" class="close" aria-hidden="true">&times;</button>', '<h3 class="popover-title"></h3>', '</div>', '<div class="popover-content"></div>', '</div>'].join('');
+  var popoverElement;
+  var calendarSelectStart;
+  var calendarSelectEnd;
+  $(document).ready(function () {
+    var savePopoverDom = document.getElementById('savePopover');
+    var calendarDom = document.getElementById('calendar');
+    $(calendarDom).fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      defaultDate: '2015-02-12',
+      navLinks: true,
+      // can click day/week names to navigate views
+      selectable: true,
+      selectHelper: true,
+      select: function select(start, end, jsEvent) {
+        closePopovers();
+        popoverElement = $(jsEvent.target);
+        $(jsEvent.target).popover({
+          title: 'the title',
+          content: function content() {
+            return '<div class="popoverContent">' + '  <input type="text" class="form-control" id="popoverContentInput"/>' + '  <button type="button" class="btn btn-primary" id="savePopover">儲存</button>' + '</div>';
+          },
+          template: popTemplate,
+          placement: 'left',
+          html: 'true',
+          trigger: 'click',
+          animation: 'true',
+          container: 'body'
+        }).popover('show');
+        calendarSelectStart = start;
+        calendarSelectEnd = end;
+      },
+      eventClick: function eventClick(calEvent, jsEvent, view) {
+        popoverElement = $(jsEvent.currentTarget);
+      },
+      eventRender: function eventRender(event, element) {
+        element.append('<span class=\'closeon fc-devare\'>X</span>');
+        element.find('.closeon').click(function () {
+          if (confirm('確認刪除?')) {
+            $('#calendar').fullCalendar('removeEvents', event._id);
+          }
+        });
+      },
+      editable: true,
+      eventLimit: true,
+      events: data
+    });
+    $(calendarDom).on('click', '.fc-next-button', function () {
+      // alert('clicked');
+      $(calendarDom).fullCalendar('removeEvents');
+      $(calendarDom).fullCalendar('addEventSource', newData);
+    });
+    $(calendarDom).on('click', '.fc-prev-button', function () {
+      // alert('clicked');
+      $(calendarDom).fullCalendar('removeEvents');
+      $(calendarDom).fullCalendar('addEventSource', data);
+    });
+    $(document).on('click', '#savePopover', function (e) {
+      var popoverContentInputDom = document.getElementById('popoverContentInput');
+
+      if (popoverContentInputDom.value) {
+        $(calendarDom).fullCalendar('renderEvent', {
+          title: popoverContentInputDom.value,
+          start: calendarSelectStart,
+          end: calendarSelectEnd
+        }, true); // stick? = true
+      }
+
+      $(calendarDom).fullCalendar('unselect');
+      closePopovers();
+    });
+    $('body').on('click', function (e) {
+      // close the popover if: click outside of the popover || click on the close button of the popover
+      if (popoverElement && (!popoverElement.is(e.target) && popoverElement.has(e.target).length === 0 && $('.popover').has(e.target).length === 0 || popoverElement.has(e.target) && e.target.id === 'closepopover')) {
+        // / $('.popover').popover('hide'); --> works
+        closePopovers();
+      }
+    });
+
+    function closePopovers() {
+      $('.popover').not(this).popover('hide');
+    }
+  });
+})();
+//# sourceMappingURL=myCalendar.js.map
--- src/js/myJobs.js
+++ src/js/myJobs.js
@@ -1,186 +1,186 @@
 var responseText = {
-  'data': [{
-    'id': '1',
-    'name': 'Tiger Nixon',
-    'position': 'System Architect',
-    'salary': '$320,800',
-    'start_date': '2011/04/25',
-    'office': 'Edinburgh'
+  data: [{
+    id: '1',
+    name: 'Tiger Nixon',
+    position: 'System Architect',
+    salary: '$320,800',
+    start_date: '2011/04/25',
+    office: 'Edinburgh'
   }, {
-    'id': '2',
-    'name': 'Garrett Winters',
-    'position': 'Accountant',
-    'salary': '$170,750',
-    'start_date': '2011/07/25',
-    'office': 'Tokyo'
+    id: '2',
+    name: 'Garrett Winters',
+    position: 'Accountant',
+    salary: '$170,750',
+    start_date: '2011/07/25',
+    office: 'Tokyo'
   }, {
-    'id': '3',
-    'name': 'Ashton Cox',
-    'position': 'Junior Technical Author',
-    'salary': '$86,000',
-    'start_date': '2009/01/12',
-    'office': 'San Francisco'
+    id: '3',
+    name: 'Ashton Cox',
+    position: 'Junior Technical Author',
+    salary: '$86,000',
+    start_date: '2009/01/12',
+    office: 'San Francisco'
   }, {
-    'id': '4',
-    'name': 'Cedric Kelly',
-    'position': 'Senior Javascript Developer',
-    'salary': '$433,060',
-    'start_date': '2012/03/29',
-    'office': 'Edinburgh'
+    id: '4',
+    name: 'Cedric Kelly',
+    position: 'Senior Javascript Developer',
+    salary: '$433,060',
+    start_date: '2012/03/29',
+    office: 'Edinburgh'
   }, {
-    'id': '5',
-    'name': 'Airi Satou',
-    'position': 'Accountant',
-    'salary': '$162,700',
-    'start_date': '2008/11/28',
-    'office': 'Tokyo'
+    id: '5',
+    name: 'Airi Satou',
+    position: 'Accountant',
+    salary: '$162,700',
+    start_date: '2008/11/28',
+    office: 'Tokyo'
   }, {
-    'id': '6',
-    'name': 'Brielle Williamson',
-    'position': 'Integration Specialist',
-    'salary': '$372,000',
-    'start_date': '2012/12/02',
-    'office': 'New York'
+    id: '6',
+    name: 'Brielle Williamson',
+    position: 'Integration Specialist',
+    salary: '$372,000',
+    start_date: '2012/12/02',
+    office: 'New York'
   }, {
-    'id': '7',
-    'name': 'Herrod Chandler',
-    'position': 'Sales Assistant',
-    'salary': '$137,500',
-    'start_date': '2012/08/06',
-    'office': 'San Francisco'
+    id: '7',
+    name: 'Herrod Chandler',
+    position: 'Sales Assistant',
+    salary: '$137,500',
+    start_date: '2012/08/06',
+    office: 'San Francisco'
   }, {
-    'id': '8',
-    'name': 'Rhona Davidson',
-    'position': 'Integration Specialist',
-    'salary': '$327,900',
-    'start_date': '2010/10/14',
-    'office': 'Tokyo'
+    id: '8',
+    name: 'Rhona Davidson',
+    position: 'Integration Specialist',
+    salary: '$327,900',
+    start_date: '2010/10/14',
+    office: 'Tokyo'
   }, {
-    'id': '9',
-    'name': 'Colleen Hurst',
-    'position': 'Javascript Developer',
-    'salary': '$205,500',
-    'start_date': '2009/09/15',
-    'office': 'San Francisco'
+    id: '9',
+    name: 'Colleen Hurst',
+    position: 'Javascript Developer',
+    salary: '$205,500',
+    start_date: '2009/09/15',
+    office: 'San Francisco'
   }, {
-    'id': '10',
-    'name': 'Sonya Frost',
-    'position': 'Software Engineer',
-    'salary': '$103,600',
-    'start_date': '2008/12/13',
-    'office': 'Edinburgh'
+    id: '10',
+    name: 'Sonya Frost',
+    position: 'Software Engineer',
+    salary: '$103,600',
+    start_date: '2008/12/13',
+    office: 'Edinburgh'
   }, {
-    'id': '11',
-    'name': 'Jena Gaines',
-    'position': 'Office Manager',
-    'salary': '$90,560',
-    'start_date': '2008/12/19',
-    'office': 'London'
+    id: '11',
+    name: 'Jena Gaines',
+    position: 'Office Manager',
+    salary: '$90,560',
+    start_date: '2008/12/19',
+    office: 'London'
   }, {
-    'id': '12',
-    'name': 'Quinn Flynn',
-    'position': 'Support Lead',
-    'salary': '$342,000',
-    'start_date': '2013/03/03',
-    'office': 'Edinburgh'
+    id: '12',
+    name: 'Quinn Flynn',
+    position: 'Support Lead',
+    salary: '$342,000',
+    start_date: '2013/03/03',
+    office: 'Edinburgh'
   }, {
-    'id': '13',
-    'name': 'Charde Marshall',
-    'position': 'Regional Director',
-    'salary': '$470,600',
-    'start_date': '2008/10/16',
-    'office': 'San Francisco'
+    id: '13',
+    name: 'Charde Marshall',
+    position: 'Regional Director',
+    salary: '$470,600',
+    start_date: '2008/10/16',
+    office: 'San Francisco'
   }, {
-    'id': '14',
-    'name': 'Haley Kennedy',
-    'position': 'Senior Marketing Designer',
-    'salary': '$313,500',
-    'start_date': '2012/12/18',
-    'office': 'London'
+    id: '14',
+    name: 'Haley Kennedy',
+    position: 'Senior Marketing Designer',
+    salary: '$313,500',
+    start_date: '2012/12/18',
+    office: 'London'
   }, {
-    'id': '15',
-    'name': 'Tatyana Fitzpatrick',
-    'position': 'Regional Director',
-    'salary': '$385,750',
-    'start_date': '2010/03/17',
-    'office': 'London'
+    id: '15',
+    name: 'Tatyana Fitzpatrick',
+    position: 'Regional Director',
+    salary: '$385,750',
+    start_date: '2010/03/17',
+    office: 'London'
   }, {
-    'id': '16',
-    'name': 'Michael Silva',
-    'position': 'Marketing Designer',
-    'salary': '$198,500',
-    'start_date': '2012/11/27',
-    'office': 'London'
+    id: '16',
+    name: 'Michael Silva',
+    position: 'Marketing Designer',
+    salary: '$198,500',
+    start_date: '2012/11/27',
+    office: 'London'
   }, {
-    'id': '17',
-    'name': 'Paul Byrd',
-    'position': 'Chief Financial Officer (CFO)',
-    'salary': '$725,000',
-    'start_date': '2010/06/09',
-    'office': 'New York'
+    id: '17',
+    name: 'Paul Byrd',
+    position: 'Chief Financial Officer (CFO)',
+    salary: '$725,000',
+    start_date: '2010/06/09',
+    office: 'New York'
   }, {
-    'id': '18',
-    'name': 'Gloria Little',
-    'position': 'Systems Administrator',
-    'salary': '$237,500',
-    'start_date': '2009/04/10',
-    'office': 'New York'
+    id: '18',
+    name: 'Gloria Little',
+    position: 'Systems Administrator',
+    salary: '$237,500',
+    start_date: '2009/04/10',
+    office: 'New York'
   }, {
-    'id': '19',
-    'name': 'Bradley Greer',
-    'position': 'Software Engineer',
-    'salary': '$132,000',
-    'start_date': '2012/10/13',
-    'office': 'London'
+    id: '19',
+    name: 'Bradley Greer',
+    position: 'Software Engineer',
+    salary: '$132,000',
+    start_date: '2012/10/13',
+    office: 'London'
   }, {
-    'id': '20',
-    'name': 'Dai Rios',
-    'position': 'Personnel Lead',
-    'salary': '$217,500',
-    'start_date': '2012/09/26',
-    'office': 'Edinburgh'
+    id: '20',
+    name: 'Dai Rios',
+    position: 'Personnel Lead',
+    salary: '$217,500',
+    start_date: '2012/09/26',
+    office: 'Edinburgh'
   }, {
-    'id': '21',
-    'name': 'Jenette Caldwell',
-    'position': 'Development Lead',
-    'salary': '$345,000',
-    'start_date': '2011/09/03',
-    'office': 'New York'
+    id: '21',
+    name: 'Jenette Caldwell',
+    position: 'Development Lead',
+    salary: '$345,000',
+    start_date: '2011/09/03',
+    office: 'New York'
   }, {
-    'id': '22',
-    'name': 'Yuri Berry',
-    'position': 'Chief Marketing Officer (CMO)',
-    'salary': '$675,000',
-    'start_date': '2009/06/25',
-    'office': 'New York'
+    id: '22',
+    name: 'Yuri Berry',
+    position: 'Chief Marketing Officer (CMO)',
+    salary: '$675,000',
+    start_date: '2009/06/25',
+    office: 'New York'
   }, {
-    'id': '23',
-    'name': 'Caesar Vance',
-    'position': 'Pre-Sales Support',
-    'salary': '$106,450',
-    'start_date': '2011/12/12',
-    'office': 'New York'
+    id: '23',
+    name: 'Caesar Vance',
+    position: 'Pre-Sales Support',
+    salary: '$106,450',
+    start_date: '2011/12/12',
+    office: 'New York'
   }, {
-    'id': '24',
-    'name': 'Doris Wilder',
-    'position': 'Sales Assistant',
-    'salary': '$85,600',
-    'start_date': '2010/09/20',
-    'office': 'Sidney'
+    id: '24',
+    name: 'Doris Wilder',
+    position: 'Sales Assistant',
+    salary: '$85,600',
+    start_date: '2010/09/20',
+    office: 'Sidney'
   }, {
-    'id': '25',
-    'name': 'Angelica Ramos',
-    'position': 'Chief Executive Officer (CEO)',
-    'salary': '$1,200,000',
-    'start_date': '2009/10/09',
-    'office': 'London'
+    id: '25',
+    name: 'Angelica Ramos',
+    position: 'Chief Executive Officer (CEO)',
+    salary: '$1,200,000',
+    start_date: '2009/10/09',
+    office: 'London'
   }, {
-    'id': '26',
-    'name': 'Gavin Joyce',
-    'position': 'Developer',
-    'salary': '$92,575',
-    'start_date': '2010/12/22',
-    'office': 'Edinburgh'
+    id: '26',
+    name: 'Gavin Joyce',
+    position: 'Developer',
+    salary: '$92,575',
+    start_date: '2010/12/22',
+    office: 'Edinburgh'
   }]
 };
 $.mockjax({
@@ -188,5 +188,16 @@ $.mockjax({
   status: 200,
   responseTime: 150,
   responseText: responseText
-});
+})(function () {
+  $('.formBox input').focusout(formBoxFunc);
+  $('.formBox textarea').focusout(formBoxFunc);
+
+  function formBoxFunc() {
+    if ($(this).val() !== '') {
+      $(this).addClass('isCompleted');
+    } else {
+      $(this).removeClass('isCompleted');
+    }
+  }
+})();
 //# sourceMappingURL=myJobs.js.map
