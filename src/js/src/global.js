81fa23a633a020de18e3e378ed66fddbd950b91c
Jacky <jacky8595125@gmail.com>
2018-10-02 17:14:49 +0800

    ...

--- src/js/src/global.js
+++ src/js/src/global.js
@@ -6,6 +6,7 @@
     '<li><a href="dragula.html"><i class="fa fa-circle-o"></i> 我的工作區</a></li>'+
     '<li><a href="myCalendar.html"><i class="fa fa-circle-o"></i> 我的日曆</a></li>'+
     '<li><a href="myJobs.html"><i class="fa fa-circle-o"></i> 我的專案</a></li>'+
-    '<li><a href="meetingManage.html"><i class="fa fa-circle-o"></i> 會議管理</a></li>'
+    '<li><a href="meetingManage.html"><i class="fa fa-circle-o"></i> 會議管理</a></li>'+
+    '<li><a href="bulletinMaintain.html"><i class="fa fa-circle-o"></i> 公布欄維護</a></li>'
   )
 })()
